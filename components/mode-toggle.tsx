3cc0c5800d98d26a11313e3ae5f0462c734a04b8
abhay-raj19 <rajpootabhay423@gmail.com>
2024-07-15 00:47:52 +0530

    feat:toggling
    Signed-off-by: abhay-raj19 <rajpootabhay423@gmail.com>

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import playSound from "../lib/PlaySound";
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      type="button"
+      size="icon"
+      className="px-2"
+      onClick={() => {
+        playSound("/theme-audio.wav");
+        setTheme(theme === "light" ? "dark" : "light");
+      }}
+    >
+      <MoonIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
+      <SunIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+    </Button>
+  );
+}
